2653fa841311d459d4886a4ee2b020b69fa77b0c
Raj Sakpal <raj.sakpal@acc.ltd>
2025-07-09 17:39:59 +0530

    3nd commit

--- githubService.js
+++ githubService.js
@@ -1,5 +1,5 @@
 import { Octokit } from "@octokit/rest";
-import analyzeDiffWithAI from "../langchainAgent.js";
+import analyzeDiffWithAI from "./langchainAgent.js";
 
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
